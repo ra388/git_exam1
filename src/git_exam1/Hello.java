4ae2fe40c213849907e78c9cca0927c73a1bb9db
Lee Sarang <ra993388@gmail.com>
2021-09-28 09:40:10 +0900

    201814026-4

--- src/git_exam1/Hello.java
+++ src/git_exam1/Hello.java
@@ -3,9 +3,9 @@ package git_exam1;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("IT융합자율학부");
-		System.out.println("201814026");
-		System.out.println("이사랑");
+		System.out.println("소속: IT융합자율학부 소프트웨어학과");
+		System.out.println("학번: 201814026");
+		System.out.println("이름: 이사랑");
 	}
 
 }
